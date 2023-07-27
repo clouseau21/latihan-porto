0cf66f7a3488ceebbc4542d2f0b15e2f220c10ef
clouseau21 <65022994+clouseau21@users.noreply.github.com>
2023-07-27 13:48:18 +0700

    Add files via upload

---
+++ js/app.js
@@ -0,0 +1,15 @@
+function contoh() {
+
+    swal({
+
+         title: "Berhasil!",
+
+         text: "Pop-up berhasil ditampilkan",
+
+         icon: "success",
+
+         button: true
+
+     });
+
+ }
